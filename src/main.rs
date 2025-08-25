fbbb24a8e2c9b4c5198950ed8bd43dd7f56462e4
benmandrew <benmandrew@gmail.com>
2025-08-25 23:56:36 +0100

    Added WindowSetup to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ggez::conf::WindowMode;
+use ggez::conf;
 use ggez::event::{self, EventLoop};
 use ggez::{Context, ContextBuilder};
 
@@ -16,11 +16,13 @@ fn init_logger() {
 
 fn init_context() -> (Context, EventLoop<()>) {
     let resources_dir = std::path::PathBuf::from("./resources");
-    let window_mode = WindowMode::default()
+    let window_mode = conf::WindowMode::default()
         .dimensions(1600.0, 1200.0)
         .resizable(true);
+    let window_setup = conf::WindowSetup::default().vsync(false).title("Munch");
     ContextBuilder::new("my_game", "Cool Game Author")
         .window_mode(window_mode)
+        .window_setup(window_setup)
         .add_resource_path(resources_dir)
         .build()
         .expect("Could not create ggez context")
